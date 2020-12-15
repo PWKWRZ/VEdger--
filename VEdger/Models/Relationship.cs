4b5696df3fdc18ed319c028e67840fb2a95366f2
kamil.weclewski <k.t.weclewski@gmail.com>
2020-12-15 13:29:04 +0100

    Modele encji dla bazy danych
    Do sprawdzenia czy zgadza sie z regulami i schematem, pytanie jak i czy laczyc UserData z odpowiedzialnymi za autentykacje

---
+++ VEdger/Models/Relationship.cs
@@ -0,0 +1,11 @@
+﻿namespace VEdger.Models
+{
+    public enum Relationship
+    {
+        seen,
+        liked,
+        matched,
+        unmatched,
+        blocked
+    }
+}
---
+++ VEdger/Models/UserData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+
+namespace VEdger.Models
+{
+    public class UserData
+    {
+        [Required]
+        [Key]
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public DateTime date_of_birth { get; set; }
+        [Timestamp]
+        public byte[] created_at { get; set; }
+        public string location { get; set; }
+        public FoodPreferenceEnum food_preference {get; set; }
+        public string description { get; set; }
+
+        //relacja  //to check if needed nawadays
+        public virtual ICollection<Photos> UserPhotos{ get; set; }
+        public virtual ICollection<MatchStatus> MatchStatus{ get; set; }
+    }
+}

e82bcf982e3eece1032d34719ccb84b0842cb93f
Radosław Zegzuła <r.zegzula.092@studms.ug.edu.pl>
2020-12-22 07:51:42 +0100

    Dodanie repositorry pattern

---
+++ VEdger/Repository/EntityRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VEdger.Repository
+{
+    public class EntityRepository<T> : IRepository<T>
+    {
+        public void Create<T>(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete<T>(T entityForDeletion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Read<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Read<T>(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Read<T>(T parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update<T>(T entityForUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VEdger/Repository/IRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VEdger.Repository
+{
+    public interface IRepository <T>
+    {
+        /// <summary>
+        /// Creates one new entity
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entity"></param>
+        void Create<T>(T entity);
+
+        /// <summary>
+        /// Returns entities
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        IEnumerable<T> Read<T>();
+
+        /// <summary>
+        /// Returns one entity by id
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        T Read<T>(int id);
+
+        /// <summary>
+        /// Returns filtered entities
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        IEnumerable<T> Read<T>(T parameters);
+
+        /// <summary>
+        /// Updates one entity
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entityForUpdate"></param>
+        void Update<T>(T entityForUpdate);
+
+        /// <summary>
+        /// Deletes one entity
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entityForDeletion"></param>
+        void Delete<T>(T entityForDeletion);
+    }
+}

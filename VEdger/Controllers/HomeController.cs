7ee3fd514b793e897adb4488e3abc4c013c66a75
Radosław Zegzuła <r.zegzula.092@studms.ug.edu.pl>
2020-12-22 07:52:06 +0100

    Dodanie przykładu wykorzystania

--- VEdger/Controllers/HomeController.cs
+++ VEdger/Controllers/HomeController.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VEdger.Models;
+using VEdger.Repository;
 
 namespace VEdger.Controllers
 {
     public class HomeController : Controller
     {
+
+          // Przykład wykorzystania 
+        //readonly IRepository<UserData> repository = new EntityRepository<UserData>();
+        //
+        //public ActionResult Index()
+        //
+        //{
+        //    var data = repository.Read<UserData>();       
+        //    return View(data);       
+        //}
+
         public ActionResult Index()
         {
             return View();
